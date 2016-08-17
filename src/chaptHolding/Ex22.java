d0f292a3f4dbd48f1ef322f1763fd96e202335a8
lnskj <anuta.0808@gmail.com>
2016-08-17 21:27:20 +0300

    ex22 almost done

--- src/chaptHolding/Ex22.java
+++ src/chaptHolding/Ex22.java
@@ -1,4 +1,5 @@
 package chaptHolding;
+
 // !!! first word doesn't add!
 
 import java.util.*;
@@ -12,42 +13,27 @@ import net.mindview.util.TextFile;
 public class Ex22 {
 
     public static void main(String[] args) {
-	List<String> words = new ArrayList<String>(
-		new TextFile("src/holding/SetOperations.java", "\\W+"));
+	List<String> words = new ArrayList<String>(new TextFile(
+		"src/holding/SetOperations.java", "\\W+"));
 	Collections.sort(words, String.CASE_INSENSITIVE_ORDER);
 	System.out.println("Sorted words to count: " + words);
 	System.out.println();
-	
+
 	Set<Word> wordSet = new HashSet<>();
-	ListIterator<String> it = words.listIterator();
-/*	while(it.hasNext()) {
-			String s = (String)it.next();
-			int count = 0;			
-			for(int i = 0; i < words.size(); i++) {
-				if(s.equals(words.get(i))) count++;
-			}
-			Word w = new Word(s, count);
-			wordObjects.add(w);
-		}*/
-	int ind = 0;
+	
 	String compared = words.get(0);
-	while (it.hasNext()) {
-	    int count = 0;
-	    for (int i = ind; i < words.size(); i++) {
-		if (compared.equals(words.get(i))) {
-		    count++;
-		    continue;
-		} else {
-		    ind = i;
-		    Word w = new Word(compared, count);
-		    wordSet.add(w);
-		    compared = words.get(i);
-		    break;
-		}
+	int count = 0;
+	for (String x : words) {
+	    if (compared.equals(x)) {
+		count++;
+	    } else {
+		
+		wordSet.add(new Word(compared,count));
+		count = 1;
+		compared = x;
 	    }
 	}
-	
-	
+
 	System.out.println("Word count: " + wordSet);
     }
 
